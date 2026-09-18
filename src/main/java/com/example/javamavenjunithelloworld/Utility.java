5255c7de344d4ed57e496e5796534538d976d3ac
swathi-r29 <swathi29rd@gmail.com>
2026-09-18 10:53:01 +0530

    SWATHIR001 - Added area of triangle function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,9 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static double areaOfTriangle(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+}
